ce10ef06db69e2b9328aa1c7ef323535033264b2
Anthony Lademan <alademan@iit.edu>
2012-07-13 01:39:34 -0500

    And the rest of it..

---
+++ get_stylesheet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Author:   Anthony Lademan
+# Date:     July 13, 2012
+# Purpose:  Automatically pull the current CSS from /r/ADHD and commit to the
+#           repo.
+
+# Variables
+PROJPATH="/home/satoshi/projects/rADHDBackup/"
+
+# Change PWD
+cd $PROJPATH
+
+# Get CSS
+wget http://www.reddit.com/r/adhd/stylesheet.css -O style.css
+
+# Add it to git
+git add style.css
+
+# Commit
+git commit -m "Nightly CSS Backup $(date)"
+
+# Push
+git push
