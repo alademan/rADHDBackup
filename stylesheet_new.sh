e127b8b7daf43d9b0bf9cb7f3402372f3ce76a0e
Anthony Lademan <alademan@iit.edu>
2012-08-21 16:00:02 -0500

    Nightly CSS Backup Tue Aug 21 16:00:02 CDT 2012

---
+++ stylesheet_new.sh
@@ -0,0 +1,16 @@
+.side h3:nth-of-type(1) 
+{ 
+  background-color: #EEEEEE;
+  background-position: 0% 50%;
+  background-repeat: no-repeat;
+  border-left: 6px solid #999999;
+  color: #333333;
+  left: 0px;
+  margin-left: 20px;
+  margin-top: 20px;
+  padding-left: 2px;
+  position: absolute;
+  text-align: left;
+  top: 50px;
+  width: 63%;
+}
